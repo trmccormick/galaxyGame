ee2b42afc4c1a5f05afef498fac8c148984cd5e9
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-03-25 12:20:21 -0400

    current updates for the cryptocurrency_mining concern unit factory and spec fo rthe computer_spec.

--- galaxy_game/app/models/concerns/cryptocurrency_mining.rb
+++ galaxy_game/app/models/concerns/cryptocurrency_mining.rb
@@ -8,19 +8,15 @@ module CryptocurrencyMining
 
   def mine_gcc
     total_gcc = 0
-    total_power_usage = 0
 
-    # Mine from computers in satellites first
-    total_gcc += mine_from_units(satellite_computers, total_power_usage)
+    computers = base_units.select { |unit| unit.is_a?(Units::Computer) }
 
-    # Then mine from directly attached computers
-    total_gcc += mine_from_units(ground_computers, total_power_usage)
+    computers.each do |computer|
+      total_gcc += computer.mine(mining_difficulty, unit_efficiency)
+    end
 
     if total_gcc > 0
-      # Update account balance
       account.update(balance: account.balance + total_gcc)
-      
-      # Update internal funds if supported
       update!(funds: funds + total_gcc) if respond_to?(:funds)
     end
     
@@ -28,33 +24,6 @@ module CryptocurrencyMining
   end
 
   private
-
-  def mine_from_units(units, total_power_usage)
-    total_gcc = 0
-
-    Array(units).each do |unit|
-      if total_power_usage + unit.mining_power <= available_power
-        total_gcc += unit.mine(mining_difficulty, unit_efficiency)
-        total_power_usage += unit.mining_power
-      else
-        break
-      end
-    end
-
-    total_gcc
-  end
-
-  def ground_computers
-    return @ground_computers if defined?(@ground_computers)
-    base_units.where(type: 'Units::Computer')
-  end
-
-  def satellite_computers
-    return @satellite_computers if defined?(@satellite_computers)
-    base_units.where(type: 'Craft::BaseCraft', craft_type: 'satellite')
-      .map { |satellite| satellite.base_units.find_by(type: 'Units::Computer') }
-      .compact
-  end
 
   def available_power
     1000 # Example power limit (can be dynamic)
@@ -68,5 +37,3 @@ module CryptocurrencyMining
     1.0 # Can be affected by maintenance status, etc
   end
 end
-
-
---
+++ galaxy_game/spec/factories/units/units.rb
@@ -0,0 +1,54 @@
+# spec/factories/units.rb
+FactoryBot.define do
+  factory :base_unit, class: 'Units::BaseUnit' do   
+    sequence(:identifier) { |n| "UNIT#{n}" }         
+    sequence(:name) { |n| "Test Unit #{n}" }
+    unit_type { "storage_unit" }
+
+    # Required associations
+    association :owner, factory: :base_settlement
+    attachable { owner }
+
+    trait :housing do
+      unit_type { "inflatable_habitat" }
+    end
+
+    trait :gas_storage do
+      unit_type { "gas_storage" }
+    end
+
+    trait :power do
+      unit_type { "solar_panel" }
+    end
+
+    trait :storage do
+      unit_type { "storage_unit" }
+    end    
+
+    trait :with_location do
+      association :location, factory: :celestial_location
+    end
+
+    trait :with_rig do
+      after(:create) do |unit|
+        create(:base_rig, attachable: unit)
+      end
+    end
+
+    trait :with_inventory do
+      after(:create) do |unit|
+        create(:inventory, storage_unit: unit)
+      end
+    end
+  end
+
+  factory :computer, class: 'Units::Computer' do
+    unit_type { 'computer' }
+    operational_data { {} }
+    
+    trait :with_upgrades do
+      mining_rate { 2.0 }
+      efficiency_upgrade { 0.5 }
+    end
+  end  
+end
