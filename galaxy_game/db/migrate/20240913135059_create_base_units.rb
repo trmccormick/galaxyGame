ce430f884792dc60e99b0b4f9a9bbd43edfb05b9
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-04-03 22:52:50 -0400

    updated base_unit migration

--- galaxy_game/db/migrate/20240913135059_create_base_units.rb
+++ galaxy_game/db/migrate/20240913135059_create_base_units.rb
@@ -1,17 +1,17 @@
+# db/migrate/20240913135059_create_base_units.rb
 class CreateBaseUnits < ActiveRecord::Migration[7.0]
   def change
     create_table :base_units do |t|
-      t.string :name
-      t.string :unit_type
-      t.integer :capacity
-      t.integer :energy_cost
-      t.integer :production_rate
-      t.string :gas_type
-      t.json :resource_requirements
-      t.json :material_list
+      t.string :identifier, null: false, index: { unique: true }      
+      t.string :name, null: false
+      t.string :unit_type, null: false
+      t.jsonb :operational_data, default: {}
+      
       t.string :location_type
       t.integer :location_id 
-      t.references :owner, polymorphic: true, null: false  
+      t.references :owner, polymorphic: true, null: false, index: true  
+      t.references :attachable, polymorphic: true, index: true
+      t.references :base_unit, foreign_key: true, index: true  # Add this line for self-referential relationship
 
       t.timestamps
     end
