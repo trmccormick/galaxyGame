09247a7ed61a6663a13bc91ad16dfd2a0f6dfe1e
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-05-02 22:45:07 -0400

    current updates integrate the new concern.

--- galaxy_game/app/models/celestial_bodies/materials/gas.rb
+++ galaxy_game/app/models/celestial_bodies/materials/gas.rb
@@ -4,6 +4,8 @@ module CelestialBodies
     # The issue is that when we inherit from ::Gas, we're inheriting its association
     # which expects the top-level Atmosphere class
     class Gas < ApplicationRecord
+      include MaterialPropertiesConcern
+      
       # Define our own association instead of inheriting from ::Gas
       self.table_name = 'gases'
       
@@ -12,13 +14,16 @@ module CelestialBodies
                  class_name: 'CelestialBodies::Spheres::Atmosphere',
                  foreign_key: 'atmosphere_id'
       
-      # Copy the needed validations and methods
-      validates :name, presence: true
+      # Remove name validation since it's now in the concern
+      # validates :name, presence: true
+      
       validates :percentage, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_nil: true
       validates :ppm, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
       validates :mass, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
       validates :molar_mass, numericality: { greater_than_or_equal_to: 0 }, presence: true
       
+      # set_molar_mass_from_material is now in the concern
+      # but we still need to call it
       before_validation :set_molar_mass_from_material
       
       # Calculate moles of gas based on the material's amount and molar mass
@@ -29,18 +34,11 @@ module CelestialBodies
       
       private
       
-      def set_molar_mass_from_material
-        if molar_mass.blank? || molar_mass == 0
-          material = Lookup::MaterialLookupService.new.find_material(name)
-          # Try both locations for molar_mass to be flexible
-          self.molar_mass = material['molar_mass'] || material.dig('properties', 'molar_mass') if material
-        end
-      end
-
-      # Determine the state of the gas based on the temperature and pressure
-      def state(temperature, pressure)
-        return 'unknown' unless material
-        material.state_at(temperature, pressure)
+      # Remove set_molar_mass_from_material since it's in the concern
+      
+      # Override the default_state method from the concern
+      def default_state
+        'gas'
       end
     end
   end
