fdfc1168f2767be036f14a584cceb63e949aa1d2
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-07-05 09:52:23 -0400

    updaed base craft factory.

--- galaxy_game/spec/factories/craft/base_crafts.rb
+++ galaxy_game/spec/factories/craft/base_crafts.rb
@@ -11,15 +11,18 @@ FactoryBot.define do
         'name' => 'Starship (Lunar Variant)',
         'craft_type' => 'transport',
         'systems' => {}, # Keep existing systems hash
-        
-        "ports": {
-          "internal_module_ports": 8,
-          "external_module_ports": 2,
-          "fuel_storage_ports": 2,
-          "unit_ports": 4,
-          "external_ports": 2,
-          "propulsion_ports": 6,
-          "storage_ports": 3
+        'operational_flags' => {
+          'autonomous' => false,
+          'human_rated' => false  # Default to non-human rated
+        },
+        'ports' => {  # Use string keys consistently
+          'internal_module_ports' => 8,
+          'external_module_ports' => 2,
+          'fuel_storage_ports' => 2,
+          'unit_ports' => 4,
+          'external_ports' => 2,
+          'propulsion_ports' => 6,
+          'storage_ports' => 3
         }
       }
     end
@@ -72,22 +75,51 @@ FactoryBot.define do
     end
 
     # Trait for wormhole stabilizers
-    trait :wormhole_stabilizer do
-      craft_name { "Wormhole Stabilization Satellite" }
-      deployed { true }
+    # trait :wormhole_stabilizer do
+    #   craft_name { "Wormhole Stabilization Satellite" }
+    #   deployed { true }
       
-      after(:create) do |craft|
-        # Set systems to online
-        data = craft.operational_data || {}
-        data['systems'] = {'stabilizer_unit' => {'status' => 'online'}}
-        craft.update_column(:operational_data, data)
-      end
-    end
+    #   after(:create) do |craft|
+    #     # Set systems to online
+    #     data = craft.operational_data || {}
+    #     data['systems'] = {'stabilizer_unit' => {'status' => 'online'}}
+    #     craft.update_column(:operational_data, data)
+    #   end
+    # end
     
     # Keep simple trait for backwards compatibility
     trait :simple do
       craft_name { "Starship" }
       craft_type { "spaceships" }
+    end
+
+    # Add a human-rated trait
+    trait :human_rated do
+      after(:create) do |craft|
+        data = craft.operational_data || {}
+        data['operational_flags'] ||= {}
+        data['operational_flags']['human_rated'] = true
+        craft.update_column(:operational_data, data)
+      end
+    end
+    
+    # Add a life_support trait
+    trait :with_life_support do
+      after(:create) do |craft|
+        data = craft.operational_data || {}
+        data['recommended_fit'] ||= {}
+        data['recommended_fit']['units'] ||= []
+        
+        life_support_units = [
+          { 'id' => 'starship_habitat_unit', 'count' => 1 },
+          { 'id' => 'waste_management_unit', 'count' => 1 },
+          { 'id' => 'co2_oxygen_production_unit', 'count' => 1 },
+          { 'id' => 'water_recycling_unit', 'count' => 1 }
+        ]
+        
+        data['recommended_fit']['units'].concat(life_support_units)
+        craft.update_column(:operational_data, data)
+      end
     end
   end
 end
