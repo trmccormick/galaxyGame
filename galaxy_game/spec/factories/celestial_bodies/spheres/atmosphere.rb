d0e8230320900ed58fb937fe13ae27e1a67722cf
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-12-18 22:19:25 -0500

    updfated atmosphere model factory and spec. 28 passing examples

--- galaxy_game/spec/factories/celestial_bodies/spheres/atmosphere.rb
+++ galaxy_game/spec/factories/celestial_bodies/spheres/atmosphere.rb
@@ -5,24 +5,24 @@ FactoryBot.define do
     pressure { 1.0 }     # Earth-like in bar
     total_atmospheric_mass { 5.0e18 } # Earth-like in kg
     
-    trait :earth_like do
+    trait :earth do
       after(:create) do |atmosphere|
-        create(:gas, atmosphere: atmosphere, name: "Nitrogen", percentage: 78)
-        create(:gas, atmosphere: atmosphere, name: "Oxygen", percentage: 21)
-        create(:gas, atmosphere: atmosphere, name: "Carbon Dioxide", percentage: 0.04)
-        create(:gas, atmosphere: atmosphere, name: "Argon", percentage: 0.93)
+        create(:gas, atmosphere: atmosphere, name: "N2", percentage: 78)
+        create(:gas, atmosphere: atmosphere, name: "O2", percentage: 21)
+        create(:gas, atmosphere: atmosphere, name: "CO2", percentage: 0.04)
+        create(:gas, atmosphere: atmosphere, name: "Ar", percentage: 0.93)
       end
     end
     
-    trait :mars_like do
+    trait :mars do
       temperature { 210 }
       pressure { 0.006 }
       total_atmospheric_mass { 2.5e16 }
       
       after(:create) do |atmosphere|
-        create(:gas, atmosphere: atmosphere, name: "Carbon Dioxide", percentage: 95)
-        create(:gas, atmosphere: atmosphere, name: "Nitrogen", percentage: 2.7)
-        create(:gas, atmosphere: atmosphere, name: "Argon", percentage: 1.6)
+        create(:gas, atmosphere: atmosphere, name: "CO2", percentage: 95)
+        create(:gas, atmosphere: atmosphere, name: "N2", percentage: 2.7)
+        create(:gas, atmosphere: atmosphere, name: "Ar", percentage: 1.6)
       end
     end
   end
