289698dcaf1b8fbef6a02858abefd04aff728e26
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2026-01-08 23:32:00 -0500

    Add Environment model validations for temperature, pressure, and humidity - 8 passing examples covering biome and celestial body associations

--- galaxy_game/spec/models/environment_spec.rb
+++ galaxy_game/spec/models/environment_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Environment, type: :model do
   let(:biome) { Biome.create(name: "Tropical Rainforest", temperature_range: "25..30", humidity_range: "70..90") }
-  let(:planet) { Planet.create(name: "Earth", size: 1.0, atmosphere: { "Nitrogen" => 78.08, "Oxygen" => 20.95 }, materials: { "Iron" => 1000 }) }
-  let(:environment) { Environment.create(biome: biome, planet: planet, temperature: 28.0, pressure: 1.0, humidity: 85.0) }
+  let!(:planet) { CelestialBodies::CelestialBody.create(identifier: "EARTH-1", name: "Earth", size: 1.0, mass: 5.972e24) }
+  let(:environment) { Environment.create(biome: biome, celestial_body: planet, temperature: 28.0, pressure: 1.0, humidity: 85.0) }
 
   describe 'validations' do
     it 'is valid with valid attributes' do
@@ -18,7 +18,7 @@ RSpec.describe Environment, type: :model do
     end
 
     it 'is not valid without a planet' do
-      environment.planet = nil
+      environment.celestial_body = nil
       expect(environment).to_not be_valid
     end
 
@@ -43,8 +43,8 @@ RSpec.describe Environment, type: :model do
       should belong_to(:biome)
     end
 
-    it 'belongs to a planet' do
-      should belong_to(:planet)
+    it 'belongs to a celestial_body' do
+      should belong_to(:celestial_body)
     end
   end
 end
