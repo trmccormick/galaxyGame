45c9d2f3f24de26724b594f3a5dcec0b4ac139cc
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2026-01-09 14:31:23 -0500

    Add material_properties_concern_spec.rb - 24 examples, 0 failures
    - Tests MaterialPropertiesConcern for material properties, molar mass, state transitions, and exotic state handling
    - Includes tests for molar_mass_from_properties, set_molar_mass_from_material, state_at with phase transitions
    - Covers exotic states like metallic_hydrogen and plasma under extreme conditions
    - Validates state helper methods (solid?, liquid?, gas?) and default_state behavior
    - Ensures proper handling of material lookup service integration

--- galaxy_game/spec/models/concerns/material_properties_concern_spec.rb
+++ galaxy_game/spec/models/concerns/material_properties_concern_spec.rb
@@ -187,4 +187,67 @@ RSpec.describe MaterialPropertiesConcern do
       expect(instance.send(:default_state)).to eq('solid')
     end
   end
+
+  describe "exotic state handling" do
+    let(:test_class) do
+      Class.new(ApplicationRecord) do
+        include MaterialPropertiesConcern
+        
+        def self.table_name
+          'geological_materials'
+        end
+        
+        attr_accessor :state, :name
+      end
+    end
+    
+    it "identifies exotic states" do
+      material = test_class.new(state: 'solid')
+      expect(material.exotic_state?).to be false
+      
+      material.state = 'metallic_hydrogen'
+      expect(material.exotic_state?).to be true
+      
+      material.state = 'plasma'
+      expect(material.exotic_state?).to be true
+      
+      material.state = 'superfluid'
+      expect(material.exotic_state?).to be true
+    end
+    
+    it "calculates phase transitions for exotic conditions" do
+      # Regular phase transition
+      material = test_class.new(name: 'Iron')
+      allow(material).to receive(:properties).and_return({
+        'properties' => {
+          'melting_point' => 1811,
+          'boiling_point' => 3134
+        }
+      })
+      
+      expect(material.state_at(2000)).to eq('liquid')
+      
+      # Hydrogen at extreme pressure
+      hydrogen = test_class.new(name: 'Hydrogen')
+      allow(hydrogen).to receive(:properties).and_return({
+        'properties' => {
+          'melting_point' => 14.01,
+          'boiling_point' => 20.28
+        }
+      })
+      
+      expect(hydrogen.state_at(300, 1_500_000)).to eq('metallic_hydrogen')
+      
+      # Material at extreme temperature
+      oxygen = test_class.new(name: 'Oxygen')
+      allow(oxygen).to receive(:properties).and_return({
+        'properties' => {
+          'melting_point' => 54.36,
+          'boiling_point' => 90.19
+        }
+      })
+      
+      expect(oxygen.state_at(12000)).to eq('plasma')
+    end
+  end
 end
