c9d67a3ae97bcb96fd723666bc270bfc4f93da8b
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2026-01-12 19:23:35 -0500

    Restore HydrosphereSimulationService from Jan 8 backup
    - Restore missing methods: calculate_state_distributions, handle_ice_melting
    - Add recursive call prevention with @simulating flag
    - Implement robust volume handling for hash and numeric data structures
    - Update state distribution to use 'vapor' key for consistency with concern
    - Fix gas factory to use correct CelestialBodies::Materials::Gas class
    - Update specs to use 'vapor' instead of 'gas' in state distribution
    - Add comprehensive documentation for HydrosphereSimulationService in docs
    - All 29 HydrosphereSimulationService tests now passing

--- galaxy_game/spec/factories/gas.rb
+++ galaxy_game/spec/factories/gas.rb
@@ -1,11 +1,11 @@
 # spec/factories/gases.rb
 FactoryBot.define do
-    factory :gas do
+    factory :gas, class: 'CelestialBodies::Materials::Gas' do
       sequence(:name) { |n| "Gas #{n}" }
       percentage { 50.0 } # Default percentage
   
-      # You might want to create an associated `celestial_body` if needed
-      association :celestial_body
+      # You might want to create an associated `atmosphere` if needed
+      association :atmosphere
   
       trait :with_high_percentage do
         percentage { 90.0 }
