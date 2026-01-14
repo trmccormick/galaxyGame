76eb7bbff0d8d06c4a6b75badad9cbe5b4721af5
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2026-01-13 21:59:01 -0500

    refactor: Use existing PlanetaryGeologicalFeatureLookupService instead of GeologicalFeatureService
    - Remove GeologicalFeatureService (redundant with existing lookup service)
    - Update CelestialBodiesController to use Lookup::PlanetaryGeologicalFeatureLookupService
    - Add format_feature helper method in controller
    - Remove geological_feature_service_spec.rb
    - Update documentation to reference existing lookup service
    - All controller tests passing (3 examples, 0 failures)

--- galaxy_game/app/controllers/celestial_bodies_controller.rb
+++ galaxy_game/app/controllers/celestial_bodies_controller.rb
@@ -76,13 +76,40 @@ class CelestialBodiesController < ApplicationController
 
   # GET /celestial_bodies/:id/geological_features
   def geological_features
-    service = GeologicalFeatureService.new(@celestial_body)
-    features = service.load_features
+    # Use the existing lookup service instead of GeologicalFeatureService
+    lookup_service = Lookup::PlanetaryGeologicalFeatureLookupService.new(@celestial_body)
+    
+    features = {
+      celestial_body: {
+        id: @celestial_body.id,
+        name: @celestial_body.name,
+        identifier: @celestial_body.identifier
+      },
+      lava_tubes: lookup_service.features_by_type('lava_tube').map { |f| format_feature(f) },
+      craters: lookup_service.features_by_type('crater').map { |f| format_feature(f) },
+      strategic_sites: lookup_service.strategic_features.map { |f| format_feature(f) }
+    }
     
     render json: features
   end
 
   private
+
+  def format_feature(feature)
+    {
+      id: feature['id'],
+      name: feature['name'],
+      type: feature['feature_type'],
+      lat: feature.dig('coordinates', 'latitude'),
+      lon: feature.dig('coordinates', 'longitude'),
+      priority: feature['priority'],
+      strategic_value: feature['strategic_value'],
+      dimensions: feature['dimensions'],
+      resources: feature['resources'],
+      tier: feature['tier'],
+      discovered: feature['discovered']
+    }
+  end
 
   def set_celestial_body
     Rails.logger.info "set_celestial_body called with params[:id] = #{params[:id]}"
