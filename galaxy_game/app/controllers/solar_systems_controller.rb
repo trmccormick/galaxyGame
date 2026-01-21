a8c8e9ca3d32a2ab21d6fd319434ee3a623be13d
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2026-01-21 13:30:52 -0500

    Implement missing index and show actions in SolarSystemsController

--- galaxy_game/app/controllers/solar_systems_controller.rb
+++ galaxy_game/app/controllers/solar_systems_controller.rb
@@ -1,2 +1,10 @@
 class SolarSystemsController < ApplicationController
+  def index
+    @solar_systems = SolarSystem.includes(:stars, :celestial_bodies).order(:name)
+  end
+
+  def show
+    @solar_system = SolarSystem.includes(:galaxy, :stars, :celestial_bodies).find(params[:id])
+    @celestial_bodies = @solar_system.celestial_bodies.includes(:atmosphere).order(:name) if @solar_system
+  end
 end
