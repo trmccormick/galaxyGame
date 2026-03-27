b2157205ff9d811fc6002589df5f2056ff7db407
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2026-03-27 10:03:07 -0400

    Fix tax_authority_spec: proper singleton memoization with cattr_accessor

--- galaxy_game/app/models/organizations/tax_authority.rb
+++ galaxy_game/app/models/organizations/tax_authority.rb
@@ -2,15 +2,11 @@ module Organizations
   class TaxAuthority < BaseOrganization
     # We use a singleton pattern for the global tax authority.
     
-    def self.instance
-      # Ensures there is only one Tax Authority record, created if necessary.
-      # Assumes a BaseOrganization exists with `find_or_create_by!`
-      begin
-        find_or_create_by!(name: 'Galactic Commerce Commission Tax Authority', identifier: 'GCC-TAX')
-      rescue ActiveRecord::StatementInvalid => e
-        # Database not ready or table doesn't exist
-        Rails.logger.warn "TaxAuthority.instance failed: #{e.message}. Database may not be initialized."
-        nil
+    class << self
+      cattr_accessor :instance, default: nil
+
+      def instance
+        @instance ||= find_or_create_by!(name: 'Galactic Commerce Commission Tax Authority', identifier: 'GCC-TAX')
       end
     end
     
