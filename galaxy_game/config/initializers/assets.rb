32160de2689eefee5076d271051e987bf1b10fdc
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2026-07-31 14:27:16 -0400

    fix: add terrain_tile_renderer to asset precompile list

--- galaxy_game/config/initializers/assets.rb
+++ galaxy_game/config/initializers/assets.rb
@@ -9,4 +9,9 @@ Rails.application.config.assets.version = "1.0"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( 
+  biome_renderer.js 
+  surface_view.js 
+  terrain_tile_renderer.js 
+  admin/monitor.js 
+)
