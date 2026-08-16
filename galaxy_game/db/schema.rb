5bfba2d52aacb5a6b88ebede144596b43575e459
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2026-08-16 13:04:38 -0400

    chore(db): update schema version + make from_settlement_id nullable on logistics_contracts
    Schema version: 2026_07_14_120000 → 2026_08_13_000000
    - Logistics contracts migration catching up (ContractCreationService)
    - from_settlement_id changed from NOT NULL to nullable

--- galaxy_game/db/schema.rb
+++ galaxy_game/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_07_14_120000) do
+ActiveRecord::Schema[7.0].define(version: 2026_08_13_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -801,7 +801,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_07_14_120000) do
   end
 
   create_table "logistics_contracts", force: :cascade do |t|
-    t.bigint "from_settlement_id", null: false
+    t.bigint "from_settlement_id"
     t.bigint "to_settlement_id", null: false
     t.string "material"
     t.decimal "quantity"
